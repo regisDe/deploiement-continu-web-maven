94dbe3b275b1e37d40f17f6b909e5090563bc461
toto <toto@titi.fr>
2019-04-30 13:35:51 +0200

    modif HelloServlet index.html

--- src/main/java/fr/afcepf/al33/jenkins/HelloServlet.java
+++ src/main/java/fr/afcepf/al33/jenkins/HelloServlet.java
@@ -27,7 +27,7 @@ public class HelloServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Hello from HelloServlet!!");
+		response.getWriter().append("Hello from HelloServlet!!!");
 	}
 
 	/**
